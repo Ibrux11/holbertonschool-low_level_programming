bfa4420e211e4e77f3dd5632791c321bcbe6410d
Ibrux11 <ibraluunge@gmail.com>
2024-02-20 10:25:40 +0100

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - code to put a message
  * Return: Always 0 
 */
-int main(void){
-    //Programming is like building a multilingual puzzle
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
 return 0;
 }
