85d2a68b096818254bf67c33fd37706d1d066186
Ibrux11 <ibraluunge@gmail.com>
2024-02-21 12:24:59 +0100

    trst

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ printf("The number is: %d\n", n);
 
 if (n > 0)
 {
-printf("%d is positive\n"n);
+printf("%d is positive\n",n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n"n);
+printf("%d is zero\n",n);
 }
 else
 {
-printf("%d negative\n"n);
+printf("%d is negative\n",n);
 }
 
 return (0);
