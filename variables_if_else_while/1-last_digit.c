1b6e1f0b6683961c7f1eedbe6c37ff027a9342f8
Ibrux11 <ibraluunge@gmail.com>
2024-02-21 16:24:05 +0100

    tesdt

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/** main -  Prints last digital number of a randomly generated number
+/**
+ *  main -  Prints last digital number of a randomly generated number
  *          and indicates it is grather than 5, equal to 0 or less than 6.
  *
  *  Return: Always 0.
@@ -9,6 +10,7 @@
 int main(void)
 {
 	int n, a;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	a = n % 10;
