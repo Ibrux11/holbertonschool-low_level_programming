0d60993e7db36ceca33adc8847369801c52e0b5f
Ibrux11 <ibraluunge@gmail.com>
2024-02-22 14:41:34 +0100

    lest

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*  main -
+*
+*  Return: Always 0.
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num == 57; num++)
+	{
+			putchar(num);
+	}
+			putchar('\n');
+
+	return (0);
+}
