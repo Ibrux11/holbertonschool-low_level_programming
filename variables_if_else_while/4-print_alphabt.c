00c7d379804d0740b397c8868a10b31786fdd46b
Ibrux11 <ibraluunge@gmail.com>
2024-02-22 12:01:38 +0100

    test few

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -4,6 +4,7 @@
 *         except q and e.
 *  Return: Always 0
 */
+
 int main(void)
 {
 	char c = 'a';
@@ -12,9 +13,8 @@ int main(void)
 	{
 		if (c != 'q' && c != 'e')
 			putchar(c);
-		}
-		c++;
 	}
+		c++;
 	putchar('\n');
 
 	return (0);
