5715b8cc338e8656ed357af6c59ff8079118dae3
Ibrux11 <ibraluunge@gmail.com>
2024-02-23 20:33:07 +0100

    tesr

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/** _islower - Checks for a lowercase caracter
+*	Return: Always 0.
+*/
+
+int _islower(int c)
+
+{
+if('a' <= c && c <= 'z')
+	return(1);
+
+
+else return(0);
+}
