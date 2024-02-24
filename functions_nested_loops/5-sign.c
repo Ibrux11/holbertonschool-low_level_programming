b8645f50e80eb63688245ab654bf226e57464bda
Ibrux11 <ibraluunge@gmail.com>
2024-02-24 20:54:32 +0100

    gh

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
-* print_sign - Prints the sign of a number
-* @n: parameter to check
-*
-Return: 1 if n is greater than zero, 0 if n is zero,
-*         -1 if n is less than zero
-*/
+ * print_sign - Prints the sign of a number
+ * @n: The number to be checked
+ *
+ * Return:1 if n is greater than zero,0 if n is zero,-1 if n is less than zero
+ */
 
 int print_sign(int n)
 {
