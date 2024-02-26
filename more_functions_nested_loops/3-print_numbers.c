e5cb6cb8fcff0f79c72bd17274cc9f7937048744
Ibrux11 <ibraluunge@gmail.com>
2024-02-26 20:39:21 +0100

    gvhk

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_numbers - prints numbers from 0 to 9
+*/
+
+void print_numbers(void)
+
+{
+	char digit;
+
+	digit = ('0');
+	while (digit <= '9')
+	{
+		_putchar(digit);
+		digit++;
+	}
+	_putchar('\n');
+}
