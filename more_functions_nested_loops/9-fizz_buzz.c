40baae81787bbb2acc3964d6662a5cf90fd6404e
Ibrux11 <ibraluunge@gmail.com>
2024-02-27 10:05:52 +0100

    hu

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+*    main - int
+*	 Return: Always 0
+*/
 
 int main(void)
 {
@@ -10,7 +14,7 @@ int main(void)
 		{
 			printf("FizzBuzz");
 		}
-		 else if (i % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -18,7 +22,7 @@ int main(void)
 		{
 			printf("Buzz");
 		}
-		 else
+		else
 		{
 			printf("%d", i);
 		}
