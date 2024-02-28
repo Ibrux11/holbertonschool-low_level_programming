483679b318e685c6152bcfce2d96cb52046124fd
Ibrux11 <ibraluunge@gmail.com>
2024-02-28 14:47:56 +0100

    bgf

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - Update the value a pointer points to 98.
+* @n: Pointer to an integer.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
