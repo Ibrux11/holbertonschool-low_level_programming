9f6e7e6f27e11ee018e5c5c4831d046ec9d62f1c
Ibrux11 <ibraluunge@gmail.com>
2024-03-06 14:20:59 +0100

    er

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,7 +23,7 @@ char *cap_string(char *);
 char *_strncpy(char *dest, char *src, int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-
+char *_strchr(char *s, char c);
 
 
 
