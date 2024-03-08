fe395860b4d194345e23392bdd3816db3ddf74a4
Ibrux11 <ibraluunge@gmail.com>
2024-03-08 16:32:03 +0100

    cm

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * @argc: Number of command line arguments
+ * @argv: Array containing the command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	if (argv != 0)
