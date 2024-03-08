ad8f8a54d05e99063f8dc962635112512d2065e5
Ibrux11 <ibraluunge@gmail.com>
2024-03-08 16:45:45 +0100

    rj

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * @argc: Number of command line arguments
- * @argv: Array containing the command line arguments
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* @argc: Number of command line arguments
+* @argv: Array containing the command line arguments
+*
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
-	if (argc == 0)
+	int i;
+
 	printf("%s\n", argv[0]);
-	printf("%d\n", argc);
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
 
 	return (0);
 }
+
