42cbdf1bf4a8328d9edea3608dce803e9a0ef8df
Ibrux11 <ibraluunge@gmail.com>
2024-03-13 13:48:10 +0100

    zzzzzz

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 
 #endif
