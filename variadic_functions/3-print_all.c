d829ffcf87ad89a3bd6a5df23a3e4b9b90299d77
Ibrux11 <ibraluunge@gmail.com>
2024-03-22 23:45:45 +0100

    aaa

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,8 +3,8 @@
 #include <stdarg.h>
 
 /**
-* print_char - prints a character
-* @args: the list of arguments
+* print_char - Prints a character.
+* @args: A list of variadic arguments.
 */
 void print_char(va_list args)
 {
@@ -12,8 +12,8 @@ void print_char(va_list args)
 }
 
 /**
-* print_integer - prints an integer
-* @args: the list of arguments
+* print_integer - Prints an integer.
+* @args: A list of variadic arguments.
 */
 void print_integer(va_list args)
 {
@@ -21,8 +21,8 @@ void print_integer(va_list args)
 }
 
 /**
-* print_float - prints a float
-* @args: the list of arguments
+* print_float - Prints a float.
+* @args: A list of variadic arguments.
 */
 void print_float(va_list args)
 {
@@ -30,8 +30,8 @@ void print_float(va_list args)
 }
 
 /**
-* print_string - prints a string
-* @args: the list of arguments
+* print_string - Prints a string.
+* @args: A list of variadic arguments.
 */
 void print_string(va_list args)
 {
@@ -44,44 +44,32 @@ void print_string(va_list args)
 }
 
 /**
-* print_all - prints anything
-* @format: list of types of arguments passed to the function
+* print_all - Prints anything.
+* @format: A list of types of arguments.
 */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	char *separator = "";
+	unsigned int i = 0;
 
-	unsigned int i = 0, j = 0;
+	char *sep = "";
 
-	print_t print[] = {
-		{"c", print_char},
-		{"i", print_integer},
-		{"f", print_float},
-		{"s", print_string},
-		{NULL, NULL}
-	};
+	print_handler_t print_handler;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		j = 0;
-		while (print[j].specifier)
+		print_handler = get_print_handler(format[i]);
+		if (print_handler)
 		{
-			if (format[i] == print[j].specifier[0])
-			{
-				printf("%s", separator);
-				print[j].printer(args);
-				separator = ", ";
-				break;
-			}
-			j++;
+			printf("%s", sep);
+			print_handler(args);
+			sep = ", ";
 		}
 		i++;
 	}
 
 	printf("\n");
-
 	va_end(args);
 }
